29d77daef4e84e30cfbabbb5b321262c10be0892
matteo0026 <marmotta200026@gmail.com>
2017-10-06 20:23:23 +0200

    Add hidl stuff

--- common.mk
+++ common.mk
@@ -109,4 +109,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapmaxfree=2m \
 dalvik.vm.extra-opts=-XX:+DisableExplicitGC
 
+# HIDL
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxys2plus-common/manifest.xml:system/vendor/manifest.xml
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.power@1.0-impl \
+    android.hardware.light@2.0-impl \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.renderscript@1.0-impl
